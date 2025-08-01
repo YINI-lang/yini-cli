63c38673a8aad6144a302c4cebefdff66d05119f
Marko K. S. <marko.seppanen@gmail.com>
2025-08-02 01:54:13 +0200

    Fixes

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,5 +1,5 @@
 // import pkg from '../../package.json'
-import { createRequire } from 'module.'
+import { createRequire } from 'module'
 
 const require = createRequire(import.meta.url)
 const pkg = require('../../package.json')
