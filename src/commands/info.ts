c86616740d58126f8471767e377eb75a4c33505a
Marko K. S. <marko.seppanen@gmail.com>
2025-08-03 23:43:00 +0200

    Corrections

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -7,13 +7,13 @@ const pkg = require('../../package.json')
 // import pkg from '../../package.json' assert { type: 'json' }
 
 export const printInfo = () => {
-    console.log(`** ${pkg.name} **`)
-    console.log(`Version:     ${pkg.version}`)
+    console.log(`** YINI CLI **`)
+    console.log(`yini-cli:    ${pkg.version}`)
     console.log(
         `yini-parser: ${pkg.dependencies['yini-parser'].replace('^', '')}`,
     )
     console.log(`Author:      ${pkg.author}`)
     console.log(`License:     ${pkg.license}`)
     console.log(`Homepage:    ${pkg.homepage}`)
-    console.log('Repo: https://github.com/YINI-lang/yini-parser-typescript')
+    console.log('Repo:        https://github.com/YINI-lang/yini-cli')
 }
