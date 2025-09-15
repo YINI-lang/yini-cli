60184732970ab4a1d1ad9667b85c37ce1f33a635
Marko K. S. <marko.seppanen@gmail.com>
2025-09-15 18:33:32 +0200

    Implemented validation --details

--- src/commands/validateCommand.ts
+++ src/commands/validateCommand.ts
@@ -3,11 +3,14 @@ import fs from 'node:fs'
 import { exit } from 'node:process'
 import YINI, {
     AllUserOptions,
+    IssuePayload,
     ResultMetadata,
     YiniParseResult,
 } from 'yini-parser'
 import { IGlobalOptions } from '../types.js'
 import { printObject, toPrettyJSON } from '../utils/print.js'
+
+const IS_DEBUG: boolean = false // For local debugging purposes, etc.
 
 // --- CLI command "validate" commandOptions --------------------------------------------------------
 export interface IValidateCommandOptions extends IGlobalOptions {
@@ -60,21 +63,22 @@ export const validateFile = (
         infos = diag!.infos.infoCount
     }
 
-    console.log()
-    console.log('isCatchedError = ' + isCatchedError)
-    console.log('TEMP OUTPUT')
-    console.log('isCatchedError = ' + isCatchedError)
-    console.log('  errors = ' + errors)
-    console.log('warnings = ' + warnings)
-    console.log(' notices = ' + notices)
-    console.log('   infor = ' + infos)
-    console.log('metadata = ' + metadata)
-    console.log(
-        'includeMetadata = ' +
-            metadata?.diagnostics?.effectiveOptions.includeMetadata,
-    )
-    console.log('commandOptions.report = ' + commandOptions?.report)
-    console.log()
+    IS_DEBUG && console.log()
+    IS_DEBUG && console.log('isCatchedError = ' + isCatchedError)
+    IS_DEBUG && console.log('TEMP OUTPUT')
+    IS_DEBUG && console.log('isCatchedError = ' + isCatchedError)
+    IS_DEBUG && console.log('  errors = ' + errors)
+    IS_DEBUG && console.log('warnings = ' + warnings)
+    IS_DEBUG && console.log(' notices = ' + notices)
+    IS_DEBUG && console.log('   infor = ' + infos)
+    IS_DEBUG && console.log('metadata = ' + metadata)
+    IS_DEBUG &&
+        console.log(
+            'includeMetadata = ' +
+                metadata?.diagnostics?.effectiveOptions.includeMetadata,
+        )
+    IS_DEBUG && console.log('commandOptions.report = ' + commandOptions?.report)
+    IS_DEBUG && console.log()
 
     if (!commandOptions.silent && !isCatchedError) {
         if (commandOptions.report) {
@@ -83,7 +87,8 @@ export const validateFile = (
             }
             assert(metadata) // Make sure there is metadata!
 
-            console.log(formatToReport(file, metadata))
+            console.log()
+            console.log(formatToReport(file, metadata).trim())
         }
 
         if (commandOptions.details) {
@@ -92,7 +97,8 @@ export const validateFile = (
             }
             assert(metadata) // Make sure there is metadata!
 
-            console.log(formatToDetails(file, metadata))
+            console.log()
+            printDetailsOnAllIssue(file, metadata)
         }
     }
 
@@ -104,6 +110,8 @@ export const validateFile = (
     if (isCatchedError) {
         errors = 1
     }
+    console.log()
+
     if (errors) {
         // red ✖
         console.error(
@@ -139,6 +147,7 @@ const formatToStatus = (
     notices: number,
     infos: number,
 ): string => {
+    const totalMsgs: number = errors + warnings + notices + infos
     let str = ``
 
     switch (statusType) {
@@ -153,7 +162,7 @@ const formatToStatus = (
             break
     }
 
-    str += ` (${errors} errors, ${warnings} warnings, ${notices + infos} total messages)`
+    str += ` (${errors} errors, ${warnings} warnings, ${totalMsgs} total messages)`
     return str
 }
 
@@ -176,21 +185,27 @@ const formatToReport = (
     fileWithPath: string,
     metadata: ResultMetadata,
 ): string => {
-    console.log('formatToReport(..)')
-    printObject(metadata)
-    console.log()
+    // console.log('formatToReport(..)')
+    // printObject(metadata)
+    // console.log()
 
     assert(metadata.diagnostics)
     const diag = metadata.diagnostics
 
+    const issuesCount: number =
+        diag.errors.errorCount +
+        diag.warnings.warningCount +
+        diag.notices.noticeCount +
+        diag.infos.infoCount
     const str = `Validation Report
 =================
 
 File      "${fileWithPath}"
+Issues:   ${issuesCount}
 
 Summary
 -------
-Mode:     '${metadata.mode}'
+Mode:     ${metadata.mode}
 Strict:   ${metadata.mode === 'strict'}
 
 Errors:   ${diag.errors.errorCount}
@@ -226,17 +241,59 @@ Byte Size:     ${metadata.source.sourceType === 'inline' ? 'n/a' : metadata.sour
     Warning at line 10, column 3: Section level skipped (0 → 2)
     Notice at line 1: Unused @yini directive
 */
-const formatToDetails = (
+const printDetailsOnAllIssue = (
     fileWithPath: string,
     metadata: ResultMetadata,
-): string => {
-    console.log('formatToDetails(..)')
-    printObject(metadata)
-    console.log(toPrettyJSON(metadata))
+): void => {
+    // console.log('printDetails(..)')
+    // printObject(metadata)
+    // console.log(toPrettyJSON(metadata))
+    // console.log()
+
+    assert(metadata.diagnostics)
+    const diag = metadata.diagnostics
+
+    console.log('Details')
+    console.log('-------')
     console.log()
 
-    const str = `Details`
+    const errors: IssuePayload[] = diag.errors.payload
+    printIssues('Error  ', 'E', errors)
 
-    return str
+    const warnings: IssuePayload[] = diag.warnings.payload
+    printIssues('Warning', 'W', warnings)
+
+    const notices: IssuePayload[] = diag.notices.payload
+    printIssues('Notice ', 'N', notices)
+
+    const infos: IssuePayload[] = diag.infos.payload
+    printIssues('Info   ', 'I', infos)
+
+    return
 }
+
 // -------------------------------------------------------------------------
+
+const printIssues = (
+    typeLabel: string,
+    prefix: string,
+    issues: IssuePayload[],
+): void => {
+    const leftPadding = '        '
+
+    issues.forEach((iss: IssuePayload, i: number) => {
+        const id: string = '#' + prefix + '-0' + (i + 1)
+        // const id: string = '' + prefix + '-0' + (i+1) + ':'
+
+        let str = `${typeLabel} [${id}]:\n`
+        str +=
+            leftPadding +
+            `At line ${iss.line}, column ${iss.column}: ${iss.message}`
+
+        if (iss.advice) str += '\n' + leftPadding + iss.advice
+        if (iss.hint) str += '\n' + leftPadding + iss.hint
+
+        console.log(str)
+        console.log()
+    })
+}
